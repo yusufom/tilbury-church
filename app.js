a51645c1e59b2f1c238462918a9e6aaf5b761838
yusufom <yusufolamilekanmuktar@gmail.com>
2026-01-20 13:49:01 +0100

    vercel deply

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const app = express();
+const compression = require("compression");
+const helmet = require("helmet");
+
 
 require("./startup/middleware")(app);
 require("./startup/condition")(app);
@@ -8,5 +11,7 @@ require("./startup/routes.js")(app);
 app.use(function(req, res, next) {
   res.render("error404");
 });
+app.use(compression());
+app.use(helmet());
 
 module.exports = app;
