bf99913b930b2008292a825b750e0de9f9d29a89
yusufom <yusufolamilekanmuktar@gmail.com>
2026-02-10 12:07:56 +0100

    static config

--- startup/middleware.js
+++ startup/middleware.js
@@ -8,10 +8,10 @@ const cors = require('cors');
 
 module.exports = app => {
   app.set("view engine", "ejs");
+  app.use(express.static(path.join(__dirname, "../public")));
 
   app.use(bodyParser.urlencoded({ extended: false }));
   app.use(bodyParser.json());
-  app.use(express.static("public/"));
   require("../auth/passport")(passport);
 
   app.use(function (req, res, next) {
